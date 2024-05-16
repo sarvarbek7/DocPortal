4cc55e6b62f4b9629a5d6be3904694cbc910108b
Sarvarbek Bakhtiyorov <bakhtiyorov.sarvar2001@gmail.com>
2024-05-16 18:01:29 +0500

    CODERUB

--- DocPortal.Api/QueryServices/IQueryService.cs
+++ DocPortal.Api/QueryServices/IQueryService.cs
@@ -3,12 +3,11 @@
 using DocPortal.Contracts.Endpoints;
 using DocPortal.Domain.Common.Entities;
 
-namespace DocPortal.Api.QueryServices
+namespace DocPortal.Api.QueryServices;
+
+public interface IQueryService<TEntity> where TEntity : class, IEntity
 {
-  public interface IQueryService<TEntity> where TEntity : class, IEntity
-  {
-    Expression<Func<TEntity, bool>>? ApplyFilterOptions(IFilterOptions<TEntity>? filterOptions);
-    ICollection<string> ApplyIncludeQueries(IIncludeQueryOptions<TEntity>? includeQueryOptions);
-    Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? ApplyOrderbyQuery(string? orderby, bool isDescending = false);
-  }
+  Expression<Func<TEntity, bool>>? ApplyFilterOptions(IFilterOptions<TEntity>? filterOptions);
+  ICollection<string> ApplyIncludeQueries(IIncludeQueryOptions<TEntity>? includeQueryOptions);
+  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? ApplyOrderbyQuery(string? orderby, bool isDescending = false);
 }
